db3beb1e87aeebfc95a4283ee92ee36bee96e9c8
rohya-28 <rohitarsude28@gmail.com>
2022-09-10 15:01:13 -0400

    b

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,21 @@ const Hero = () => (
         </div>
 
         <h1 className='font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'> Payment Method </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}></p>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus animi eaque odio. Libero, magnam. Quibusdam dolore inventore, repellat expedita fugiat incidunt neque quae porro aliquam consequuntur impedit quis, corporis autem.</p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="robot" className='w-[100%] h-[100%] relative z-[5]' />
+
+        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+        <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient' />
+        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+        
+        
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+         <GetStarted />
       </div>
 
   </section>
